432e2b38945d13141ea074d4bf777b481a8d8b8b
Albert Belger <albert.belger@gmail.com>
2022-01-29 19:06:45 +0200

    bot start

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-const express = require('express');
-const app = express();
+const { Telegraf } = require("telegraf");
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-    res.send({ message: 'Hello WWW!' });
+// Создать бота с полученным ключом
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN_EDU);
+
+
+// Обработчик начала диалога с ботом
+bot.start((ctx) =>
+  ctx.reply(
+    `Приветствую, ${
+       ctx.from.first_name ? ctx.from.first_name : "хороший человек"
+    }! Набери /help и увидишь, что я могу.`
+  )
+);
+
+// Обработчик команды /help
+bot.help((ctx) => ctx.reply("Справка в процессе"));
+
+// Обработчик команды /whoami
+bot.command("whoami", (ctx) => {
+  const { id, username, first_name, last_name } = ctx.from;
+  return ctx.replyWithMarkdown(`Кто ты в телеграмме:
+*id* : ${id}
+*username* : ${username}
+*Имя* : ${first_name}
+*Фамилия* : ${last_name}
+*chatId* : ${ctx.chat.id}`);
 });
 
-app.listen(8000, () => {
-    console.log('Application listening on port 8000!');
+// Обработчик команды /whoami
+bot.command("tour", (ctx) => {
+    return ctx.replyWithMarkdown(`
+    Здесь будет справка по турам.
+    `);
+  });
+
+
+// Обработчик простого текста
+bot.on("text", (ctx) => {
+  return ctx.reply(ctx.message.text);
 });
+
+// Запуск бота
+bot.launch();
